080d35b121945e0e7958081c822eef3722232de7
Aponovi <elfi.krupsky@gmail.com>
2022-05-12 16:55:13 +0200

    add authentication directory

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'app',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -122,5 +123,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.auth.JWTAuthentication',),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
--- project/urls.py
+++ project/urls.py
@@ -27,7 +27,7 @@ router.register('project', ProjectViewset, basename='project')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('authentication.urls')),
     path('api/', include(router.urls)),
 ]
